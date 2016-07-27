ca2615e6a38ac9f974283269ae7b432dcf15537d
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2016-07-27 17:04:01 +0100

    Initial commit

---
+++ slack.py
@@ -0,0 +1,29 @@
+import logging
+import os
+import pwd
+import datetime
+from opsdroid.message import Message
+from slackclient import SlackClient
+
+class ConnectorSlack:
+
+    def __init__(self, config):
+        """ Setup the connector """
+        logging.debug("Loaded slack connector")
+        self.token = config["api-token"]
+        self.sc = SlackClient(token)
+        self.name = "slack"
+
+    def connect(self, opsdroid):
+        """ Connect to the chat service """
+        logging.debug("Connecting to slack")
+        if self.sc.rtm_connect():
+            while True:
+                print self.sc.rtm_read()
+                time.sleep(1)
+        else:
+            print "Connection Failed, invalid token?"
+
+    def respond(self, message):
+        """ Respond with a message """
+        logging.debug("Responding with: " + message)
