b1508ef991d1a50b24d17aea1e93f0b0bcdbc743
Jacob Tomlinson <jacob@jacobtomlinson.co.uk>
2016-11-10 14:59:45 +0000

    Log again

--- slack.py
+++ slack.py
@@ -55,6 +55,7 @@ class ConnectorSlack(Connector):
         while True:
             content = await self.ws.recv()
             m = json.loads(content)
+            logger.debug(m)
             if "type" in m and m["type"] == "message" and "user" in m:
 
                 # Ignore bot messages
